8b4d94b284fb015fcd7765231434a838874aed4a
CristianVT <cvereaut1@upao.edu.pe>
2025-10-10 20:46:40 -0500

    feat(variables): agregar y ajustar variables de región, perfil y entorno

--- Iac/variables.tf
+++ Iac/variables.tf
@@ -17,3 +17,21 @@ variable "environment" {
   description = "El entorno para los recursos"
   type        = string
 }
+
+variable "lambda_runtime" {
+  description = "Runtime para las funciones Lambda"
+  type        = string
+  default     = "nodejs18.x"
+}
+
+variable "lambda_handler" {
+  description = "Handler de las funciones Lambda"
+  type        = string
+  default     = "index.handler"
+}
+
+variable "table_name" {
+  description = "Nombre de la tabla DynamoDB"
+  type        = string
+  default     = "Cursos"
+}
